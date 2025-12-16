54fb5875e0c2095b8ecf2e84393515642195dd11
Raihan Rafid <raihanrfd1@gmail.com>
2025-12-16 14:04:26 +0700

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  // static export mode (replaces legacy next export)
   output: "export",
+  // for project pages (https://user.github.io/repo) set basePath & assetPrefix
+  // basePath: repoName ? `/${repoName}` : "",
+  // assetPrefix: repoName ? `/${repoName}/` : "",
+  // export to folder/index.html (works well on GH Pages)
   trailingSlash: true,
+  // disable built-in image optimizer (not available on static hosts)
   images: {
     domains: ['raw.githubusercontent.com'],
     unoptimized: true, // Disable Image Optimization
   },
-  // webpack: (config, { isServer }) => {
-  //   if (isServer) {
-  //     // Ignore 'self' references for the server-side build
-  //     config.resolve.fallback = {
-  //       ...config.resolve.fallback,
-  //       self: false, // Avoid 'self' reference in server-side code
-  //     };
-  //   }
-  //   return config;
-  // },
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
